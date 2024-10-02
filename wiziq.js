ea2e84327163dc2f420c6f175647cda71c8917eb
Jay-K04 <jeremiahkadiri24@gmail.com>
2024-10-02 13:44:18 +0100

    first Project

---
+++ wiziq.js
@@ -0,0 +1,81 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides fieldset");
+let slideIndex = 0;
+
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded",initializeSlider)
+function initializeSlider(){
+    var x = window.matchMedia("(max-width: 500px)");
+    if (slides.length > 0){
+        if (!(x.matches)) { // If media query matches
+            slides[slideIndex].classList.add("displaySlide")
+            slides[slideIndex + 1].classList.add("displaySlide")
+          } else {
+            slides[slideIndex].classList.add("displaySlide")
+          }
+        intervalId = setInterval(nextSlide,3000);
+    }
+}
+
+function showSlide(index){
+      
+    if (index >= slides.length){
+        slideIndex = 0;
+    }
+    else if (index < 0){
+        slideIndex = slides.length - 1 ;
+    }
+
+    slides.forEach (slide =>{
+        slide.classList.remove("displaySlide");
+    });
+    var x = window.matchMedia("(max-width: 500px)");
+    if (!(x.matches)) { // If media query matches
+        slides[slideIndex].classList.add("displaySlide")
+        slides[slideIndex + 1].classList.add("displaySlide")
+      } else {
+        slides[slideIndex].classList.add("displaySlide")
+      }
+    
+}
+
+function prevSlide(){
+    clearInterval(intervalId);  
+    slideIndex --;
+    showSlide(slideIndex)
+
+}
+
+function nextSlide(){
+    slideIndex ++;
+    showSlide(slideIndex)
+
+}
+
+
+let mybutton = document.getElementById("back-to-top");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+let currentSlide = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelector('.slides1');
+    const totalSlides = document.querySelectorAll('.slide1').length;
+
+    currentSlide = (currentSlide + step + totalSlides) % totalSlides;
+    const slideWidth = document.querySelector('.slide1').clientWidth;
+    slides.style.transform = `translateX(${-currentSlide * slideWidth}px)`;
+}
+
+
+
